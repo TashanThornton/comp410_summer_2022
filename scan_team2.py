aa480ab2d1d2cc190303aabdeb10233c9eaae385
Drdj11 <Darius.Daniel35@gmail.com>
2022-08-02 17:37:12 -0400

    Update scan_team2.py
    added address and phone # to text list

--- scan_team2.py
+++ scan_team2.py
@@ -2,6 +2,8 @@ from scan import scan_files, get_file_text
 from team2_pii import find_us_ssn
 from team2_pii import find_credit_card_number
 from team2_pii import find_dob
+from team2_pii import find_us_address
+from team2_pii import find_us_phone_numbers
 
 file_list = scan_files()
 for f in file_list:
@@ -11,8 +13,10 @@ for f in file_list:
         ssn_found = find_us_ssn(t)
         ccn_found = find_credit_card_number(t)
         dob_found = find_dob(t)
+        add_found = find_us_address(t)
+        phone_found = find_us_phone_numbers(t)
         
-        if ssn_found or ccn_found or dob_found:
+        if ssn_found or ccn_found or dob_found or add_found or phone_found:
             print("PII: " + f + ": " + t)
         else:
             print("NO_PII: " + f + ": " + t)
